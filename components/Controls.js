0f75beade94f2f8ad18165b55e519722486d2dd7
Timothy Hyatt <tim@Inizios-MacBook-Pro.local>
2018-06-19 13:39:54 -0700

    removed required

--- components/Controls.js
+++ components/Controls.js
@@ -196,7 +196,7 @@ Controls.propTypes = {
   currentTime: PropTypes.number.isRequired,
   duration: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  logo: PropTypes.string.isRequired,
+  logo: PropTypes.string,
   theme: PropTypes.object.isRequired
 }
 

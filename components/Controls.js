07c379c78b9e472ad0a9dd8f0a5966184ebc86a4
Jeffrey Villamin <jeffmon@gmail.com>
2018-07-31 18:09:03 -1000

    Remove need for logo prop

--- components/Controls.js
+++ components/Controls.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import {
   View,
   Animated,
   StyleSheet,
   TouchableWithoutFeedback as Touchable
-} from 'react-native'
-import {
-  PlayButton,
-  ControlBar,
-  Loading,
-  TopBar,
-  ProgressBar
-} from './'
+} from "react-native";
+import { PlayButton, ControlBar, Loading, TopBar, ProgressBar } from "./";
 
 const styles = StyleSheet.create({
   container: {
@@ -22,39 +16,39 @@ const styles = StyleSheet.create({
   flex: {
     flex: 1
   }
-})
+});
 
 class Controls extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       hideControls: false,
       seconds: 0,
       seeking: false
-    }
-    this.animControls = new Animated.Value(1)
-    this.scale = new Animated.Value(1)
-    this.progressbar = new Animated.Value(2)
+    };
+    this.animControls = new Animated.Value(1);
+    this.scale = new Animated.Value(1);
+    this.progressbar = new Animated.Value(2);
   }
 
   componentDidMount() {
-    this.setTimer()
+    this.setTimer();
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
   }
 
   onSeek(pos) {
-    this.props.onSeek(pos)
+    this.props.onSeek(pos);
     if (!this.state.seeking) {
-      this.setState({ seeking: true })
+      this.setState({ seeking: true });
     }
   }
 
   onSeekRelease(pos) {
-    this.props.onSeekRelease(pos)
-    this.setState({ seeking: false, seconds: 0 })
+    this.props.onSeekRelease(pos);
+    this.setState({ seeking: false, seconds: 0 });
   }
 
   setTimer() {
@@ -62,47 +56,52 @@ class Controls extends Component {
       switch (true) {
         case this.state.seeking:
           // do nothing
-          break
+          break;
         case this.props.paused:
-          if (this.state.seconds > 0) this.setState({ seconds: 0 })
-          break
+          if (this.state.seconds > 0) this.setState({ seconds: 0 });
+          break;
         case this.state.hideControls:
-          break
+          break;
         case this.state.seconds > 3:
-          this.hideControls()
-          break
+          this.hideControls();
+          break;
         default:
-          this.setState({ seconds: this.state.seconds + 1 })
+          this.setState({ seconds: this.state.seconds + 1 });
       }
-    }, 1000)
+    }, 1000);
   }
 
   showControls() {
     this.setState({ hideControls: false }, () => {
-      this.progressbar.setValue(2)
+      this.progressbar.setValue(2);
       Animated.parallel([
         Animated.timing(this.animControls, { toValue: 1, duration: 200 }),
         Animated.timing(this.scale, { toValue: 1, duration: 200 })
-      ]).start()
-    })
+      ]).start();
+    });
   }
 
   hideControls() {
     Animated.parallel([
       Animated.timing(this.animControls, { toValue: 0, duration: 200 }),
       Animated.timing(this.scale, { toValue: 0.25, duration: 200 })
-    ]).start(() => this.setState({ hideControls: true, seconds: 0 }))
+    ]).start(() => this.setState({ hideControls: true, seconds: 0 }));
   }
 
   hiddenControls() {
-    Animated.timing(this.progressbar, { toValue: 0, duration: 200 }).start()
+    Animated.timing(this.progressbar, { toValue: 0, duration: 200 }).start();
     return (
       <Touchable style={styles.container} onPress={() => this.showControls()}>
-        <Animated.View style={[styles.container, { paddingBottom: this.progressbar }]}>
-          <ProgressBar theme={this.props.theme.progress} progress={this.props.progress} />
+        <Animated.View
+          style={[styles.container, { paddingBottom: this.progressbar }]}
+        >
+          <ProgressBar
+            theme={this.props.theme.progress}
+            progress={this.props.progress}
+          />
         </Animated.View>
       </Touchable>
-    )
+    );
   }
 
   loading() {
@@ -110,7 +109,7 @@ class Controls extends Component {
       <View style={styles.container}>
         <Loading theme={this.props.theme.loading} />
       </View>
-    )
+    );
   }
 
   displayedControls() {
@@ -128,13 +127,15 @@ class Controls extends Component {
       duration,
       theme,
       inlineOnly
-    } = this.props
+    } = this.props;
 
-    const { center, ...controlBar } = theme
+    const { center, ...controlBar } = theme;
 
     return (
       <Touchable onPress={() => this.hideControls()}>
-        <Animated.View style={[styles.container, { opacity: this.animControls }]}>
+        <Animated.View
+          style={[styles.container, { opacity: this.animControls }]}
+        >
           <TopBar
             title={title}
             logo={logo}
@@ -142,7 +143,9 @@ class Controls extends Component {
             onMorePress={() => onMorePress()}
             theme={{ title: theme.title, more: theme.more }}
           />
-          <Animated.View style={[styles.flex, { transform: [{ scale: this.scale }] }]}>
+          <Animated.View
+            style={[styles.flex, { transform: [{ scale: this.scale }] }]}
+          >
             <PlayButton
               onPress={() => this.props.togglePlay()}
               paused={paused}
@@ -167,15 +170,15 @@ class Controls extends Component {
           />
         </Animated.View>
       </Touchable>
-    )
+    );
   }
 
   render() {
-    if (this.props.loading) return this.loading()
+    if (this.props.loading) return this.loading();
     if (this.state.hideControls) {
-      return this.hiddenControls()
+      return this.hiddenControls();
     }
-    return this.displayedControls()
+    return this.displayedControls();
   }
 }
 
@@ -196,8 +199,7 @@ Controls.propTypes = {
   currentTime: PropTypes.number.isRequired,
   duration: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  logo: PropTypes.string.isRequired,
   theme: PropTypes.object.isRequired
-}
+};
 
-export { Controls }
+export { Controls };
--- components/TopBar.js
+++ components/TopBar.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import {
-  View,
-  StyleSheet,
-  Text,
-  Image
-} from 'react-native'
+import { View, StyleSheet, Text, Image } from "react-native";
 
-import LinearGradient from 'react-native-linear-gradient'
-import { ToggleIcon } from './'
-import { checkSource } from './utils'
+import LinearGradient from "react-native-linear-gradient";
+import { ToggleIcon } from "./";
+import { checkSource } from "./utils";
 
-const backgroundColor = 'transparent'
+const backgroundColor = "transparent";
 
 const styles = StyleSheet.create({
   container: {
     height: 35,
-    justifyContent: 'center'
+    justifyContent: "center"
   },
   row: {
-    flexDirection: 'row',
-    alignSelf: 'center',
-    alignItems: 'center'
+    flexDirection: "row",
+    alignSelf: "center",
+    alignItems: "center"
   },
   title: {
     flex: 1,
@@ -36,20 +31,23 @@ const styles = StyleSheet.create({
     height: 25,
     width: 25
   }
-})
+});
 
-const TopBar = (props) => {
-  const {
-    logo,
-    more,
-    title,
-    theme,
-    onMorePress
-  } = props
+const TopBar = props => {
+  const { logo, more, title, theme, onMorePress } = props;
   return (
-    <LinearGradient colors={['rgba(0,0,0,0.75)', 'rgba(0,0,0,0)']} style={styles.container}>
+    <LinearGradient
+      colors={["rgba(0,0,0,0.75)", "rgba(0,0,0,0)"]}
+      style={styles.container}
+    >
       <View style={styles.row}>
-        { logo && <Image style={styles.logo} resizeMode="contain" {...checkSource(logo)} />}
+        {logo && (
+          <Image
+            style={styles.logo}
+            resizeMode="contain"
+            {...checkSource(logo)}
+          />
+        )}
         <Text
           style={[styles.title, { color: theme.title }]}
           numberOfLines={1}
@@ -57,7 +55,7 @@ const TopBar = (props) => {
         >
           {title}
         </Text>
-        { more &&
+        {more && (
           <ToggleIcon
             style={styles.more}
             onPress={() => onMorePress()}
@@ -67,18 +65,17 @@ const TopBar = (props) => {
             theme={theme.more}
             size={25}
           />
-        }
+        )}
       </View>
     </LinearGradient>
-  )
-}
+  );
+};
 
 TopBar.propTypes = {
   title: PropTypes.string.isRequired,
-  logo: PropTypes.string.isRequired,
   more: PropTypes.bool.isRequired,
   onMorePress: PropTypes.func.isRequired,
   theme: PropTypes.object.isRequired
-}
+};
 
-export { TopBar }
+export { TopBar };

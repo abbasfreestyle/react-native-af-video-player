4c7352b5123dd15c2e8c0d38ce0691b4a74bceb2
hongsa <hsasung@gmail.com>
2019-06-12 00:29:47 +0900

    chore(loading): 로딩바 수정

--- components/Loading.js
+++ components/Loading.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
   View,
-  Easing,
   StyleSheet,
-  Animated
+  ActivityIndicator,
 } from 'react-native'
 
 const styles = StyleSheet.create({
@@ -13,70 +12,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  line: {
-    height: 5,
-    width: 75
-  }
 })
 
 class Loading extends Component {
-  constructor() {
-    super()
-    this.anim = {
-      width: new Animated.Value(10),
-      translateX: new Animated.Value(-50)
-    }
-  }
-
-  componentDidMount() {
-    Animated.loop(Animated.parallel([
-      Animated.sequence([
-        Animated.timing(this.anim.width, {
-          toValue: 75,
-          easing: Easing.back(1),
-          duration: 750
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 10,
-          // easing: Easing.back(2),
-          duration: 250
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 75,
-          easing: Easing.back(1),
-          duration: 750
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 10,
-          // easing: Easing.back(2),
-          duration: 250
-        })
-      ]),
-      Animated.sequence([
-        Animated.timing(this.anim.translateX, { toValue: 50, easing: Easing.back(1), duration: 1000 }),
-        Animated.timing(this.anim.translateX, { toValue: -50, easing: Easing.back(1), duration: 1000 })
-      ])
-    ])).start()
-  }
-
   render() {
-    const { translateX, width } = this.anim
     if (this.props.loading) {
       return (
         <View style={styles.container}>
-          <Animated.View style={[
-            styles.line,
-            {
-              backgroundColor: this.props.theme,
-              width,
-              transform: [{ translateX }]
-            }
-          ]}
-          />
+          <ActivityIndicator size="large"/>
         </View>
       )
     }
-    return null
+    return null;
   }
 }
 
